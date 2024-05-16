ed94132122dff81ac88d0a4096870d119aab5b58
krishnaprasad k <prasadkrishna1189@gamil.com>
2024-05-16 21:59:28 +0530

    ngSwitch compeleted

---
+++ hello-world/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-hello-world',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css'
+})
+export class HelloWorldComponent {
+  //  Title = "kpk works"
+  //  firstName = "Sachin"
+  //  lastName = "Tendulkar"
+  //  team = "Mumbai Indians"
+  //  name = "krishnaprasad"
+  //  getName(){
+  //   return this.name
+  //  }
+
+  //  checking = "this is working"
+  //  isDisabled = false
+
+  //  clickCounted = 0
+  //  count = 0
+  //  getCount(){
+  //   this.clickCounted++
+  //  }
+
+  // value = ""
+  // value1 = ""
+  // handleInput(event:any){
+  //   this.value = (event.target as HTMLInputElement).value
+  //   // this.value = event.target.value
+  // }
+
+
+  // Title = "list of movies"
+
+  // movies= [
+  //   "zootpia",
+  //   "Batman vs superman",
+  //   "herry ptter",
+  //   "x - men",
+  //   "final destination"
+  // ]
+
+//  moviesWithDirectors = [
+//     { title: "Zootopia", director: "Byron Howard, Rich Moore" },
+//     { title: "Batman vs Superman", director: "Zack Snyder" },
+//     { title: "Harry Potter", director: "Chris Columbus" },
+//     { title: "X-Men", director: "Bryan Singer" },
+//     { title: "Final Destination", director: "James Wong" }
+//   ];
+
+num = 0;
+
+}
